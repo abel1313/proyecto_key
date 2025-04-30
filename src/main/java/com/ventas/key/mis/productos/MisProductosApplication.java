30a6f30cee77a76aa3981c614c484ac95be8c201
abel1313 <56236714+abel1313@users.noreply.github.com>
2025-04-29 22:44:40 -0600

    first commit

---
+++ src/main/java/com/ventas/key/mis/productos/MisProductosApplication.java
@@ -0,0 +1,13 @@
+package com.ventas.key.mis.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisProductosApplication.class, args);
+	}
+
+}
